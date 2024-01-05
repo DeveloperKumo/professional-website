3d2e5be0b6f667a0c74b639515c73fe51af77df7
Kumo <developerkumo@gmail.com>
2024-01-04 23:06:21 -0500

    Started adding Mobile focused Tailwind classes and sections

--- main.go
+++ main.go
@@ -14,4 +14,3 @@ func main() {
 
 	app.Listen(":6969")
 }
- 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,21 @@ const { icestackPlugin } = require("@icestack/tailwindcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**.{html,js}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        icestackPlugin({
-            // Absolute path to the loading directory
-            loadDirectory: path.resolve(__dirname, "my-ui"),
-            // Whether to inject tailwindcss theme config
-            // When true, the generator's primary success warning colors will enter the tailwindcss.theme.extend configuration
-            //You can only use bg-primary / text-success
-            //Default value: false
-            loadConfig: true,
-            // ...
-        }),
-    ],
+  content: ["./pages/**.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    icestackPlugin({
+      // Absolute path to the loading directory
+      loadDirectory: path.resolve(__dirname, "my-ui"),
+      // Whether to inject tailwindcss theme config
+      // When true, the generator's primary success warning colors will enter the tailwindcss.theme.extend configuration
+      //You can only use bg-primary / text-success
+      //Default value: false
+      loadConfig: true,
+      // ...
+    }),
+  ],
+  important: true,
 };
